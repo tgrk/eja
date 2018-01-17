8dabfb1e8f73ce422ed2f74a91475dfbf1c3cc39
Martin Wiso <martin@wiso.cz>
2018-01-17 19:53:51 +0100

    Improve error object serialization

--- src/eja_error.erl
+++ src/eja_error.erl
@@ -15,30 +15,26 @@
 
 -spec serialize([map()]) -> map().
 serialize(Errors) ->
-  #{<<"errors">> => [build_error(E) || E <- Errors]}.
+  #{<<"errors">> => [drop_empty(build_error(E)) || E <- Errors]}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
 build_error(Error) when is_map(Error) ->
-  drop_empty(
-    build_error(
-      {
-        maps:get(title,  Error, <<"Unknown">>)
-      , maps:get(status, Error, <<>>)
-      , maps:get(detail, Error, <<>>)
-      , maps:get(params, Error, #{})
-      }
-    )
+  build_error(
+    { maps:get(title,  Error, <<"Unknown">>)
+    , maps:get(status, Error, <<>>)
+    , maps:get(detail, Error, <<>>)
+    , maps:get(params, Error, #{})
+    }
   );
-
 build_error({Title, Detail}) ->
   build_error({Title, <<>>, Detail});
 build_error({Title, Status, Detail}) ->
   build_error({Title, Status, Detail, #{}});
 build_error({Title, Status, Detail, Params}) ->
-  maybe_append_fields(
+  maybe_append_params(
     #{  <<"title">>  => Title
       , <<"detail">> => Detail
       , <<"status">> => Status
@@ -49,18 +45,26 @@ build_error({Title, Status, Detail, Params}) ->
 drop_empty(ErrorObject) ->
   maps:filter(fun (_K, <<>>) -> false;
                   (_K, undefined) -> false;
-                  (_K, _V) -> true end, ErrorObject).
+                  (_K, _V) -> true
+              end, ErrorObject).
 
-maybe_append_fields(Error, Params) ->
-  maps:fold(fun (K, V, Acc) -> append_field(K, V, Acc) end, Error, Params).
+maybe_append_params(Error, Params) ->
+  maps:fold(fun (K, V, Acc) -> append_params(K, V, Acc) end, Error, Params).
 
-append_field(status, Value, Error) ->
-  maps:put(<<"status">>, Value, Error);
-append_field(meta, Value, Error) ->
-  maps:put(<<"meta">>, #{<<"meta">> => Value}, Error);
-append_field(source, Value, Error) when is_map(Value) ->
-  maps:put(<<"source">>, Value, Error);
-append_field(source, Value, Error) ->
+append_params(meta, Value, Error) when is_map(Value) ->
+  maps:put(<<"meta">>, #{<<"meta">> => ensure_binary(Value)}, Error);
+append_params(source, Value, Error) when is_map(Value) ->
+  maps:put(<<"source">>, ensure_binary(Value), Error);
+append_params(source, Value, Error) when is_binary(Value) ->
   maps:put(<<"source">>, #{<<"pointer">> => Value}, Error);
-append_field(_Field, _Value, Error) ->
-    Error.
+append_params(_Field, _Value, Error) ->
+  Error.
+
+ensure_binary(Map) when is_map(Map) ->
+  maps:fold(fun (K, V, Acc) when is_atom(K) ->
+                  maps:put(atom_to_binary(K, latin1), V, Acc);
+                (K, V, Acc) when is_list(K) ->
+                  maps:put(list_to_binary(K), V, Acc);
+                (K, V, Acc) ->
+                  maps:put(K, V, Acc)
+            end, #{}, Map).
